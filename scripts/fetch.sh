619149a958c63e423bf98ccfd1e38f735d79cc2c
Ingimar E <inkimar@gmail.com>
2018-04-26 14:51:28 +0200

    a test-script for fetching a version of Tomcat

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -1,3 +1,3 @@
-TOMCAT_VERSION=7.0.69
-#TOMCAT_VERSION=7.0.76
+#TOMCAT_VERSION=7.0.69
+TOMCAT_VERSION=7.0.76
 wget  --no-cookies https://archive.apache.org/dist/tomcat/tomcat-7/v${TOMCAT_VERSION}/bin/apache-tomcat-${TOMCAT_VERSION}.tar.gz
