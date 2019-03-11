bdeb4e1e81dc0c10007c1a52449171b0a44935f4
Ingimar Erlingsson <inkimar@gmail.com>
2019-03-11 15:38:29 +0100

    removing SQL for older version

--- mysql_autoload/Bug10158_Remove_PcrPrimer_seq_field_validation.sql
+++
@@ -1,9 +0,0 @@
-/*
-Filename:  Bug10158_Remove_PcrPrimer_seq_field_validation.sql
-Purpose:   Sets PcrPrimer Seq field default value to empty string
-Date:      2017-11-07
-Author:    Satpal Bilkhu
-*/
-
-alter table PcrPrimers modify column Seq varchar(255) NOT NULL DEFAULT '';
-alter table PcrPrimers_AUD modify column Seq varchar(255);
